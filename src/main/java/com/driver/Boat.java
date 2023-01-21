2d00052e7abf58afdab256a27b4cbf01d747f325
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-01-21 15:47:13 +0530

    Update Boat.java

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,19 +1,22 @@
 package com.driver;
 
-public class Boat implements WaterVehicle {
+public class Boat implements WaterVehicle{
+
     private String name;
-    private  int capacity;
-    Boat(String name,int capacity)
-    {
-        this.name=name;
-        this.capacity=capacity;
+    private int capacity;
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
     }
-    public void getVehicleName()
-    {
-        return name;
+
+    @Override
+    public String getVehicleName() {
+        return this.name;
     }
-    public void getVehicleCapacity()
-    {
-        return capacity;
+
+    @Override
+    public int getVehicleCapacity() {
+        return this.capacity;
     }
 }
